f87b0307836a5388c29bb1d96fa875b5d203e130
Christopher Firgens <Christopher.Firgens@uline.com>
2020-01-20 09:31:09 -0600

    added weather component

---
+++ src/components/weather.component.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Weather = () => {
+    return (
+        <div className="container">
+            <h1>Weather App</h1>
+        </div>
+    );
+}
+
+export default Weather;
