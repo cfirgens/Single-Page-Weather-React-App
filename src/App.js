974c27ea4500bbbd421bbbe61d20584ee84026fe
Christopher Firgens <Christopher.Firgens@uline.com>
2020-01-20 11:17:57 -0600

    added api key for openweather api; changed from statless component to state component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,36 @@ import Weather from './components/weather.component';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'weather-icons/css/weather-icons.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Weather />
-    </div>
-  );
+
+const API_key = process.env.Weather_API_Key;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+    this.getWeather();
+  }
+
+  getWeather = async () => {
+    const api_call = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid='+API_key);
+    
+    const response = await api_call.json();
+
+    console.log(response);
+    
 }
 
+
+  render() { 
+    return ( 
+      <div className="App">
+      <Weather />
+      </div>
+      
+     );
+  }
+}
+ 
 export default App;
+
+
